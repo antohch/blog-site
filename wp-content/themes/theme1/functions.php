2f43cbef64fb5aa03c4268ec2849472f6879840d
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-12-26 17:12:55 +0300

    Работал с комментами

--- wp-content/themes/theme1/functions.php
+++ wp-content/themes/theme1/functions.php
@@ -150,15 +150,19 @@ function mytheme_comment($comment, $args, $depth) {
 		$tag = 'li';
 		$add_below = 'div-comment';
 	}
+
+	
 ?>
-	<<?php echo $tag ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent comment-list' ) ?> id="comment-<?php comment_ID() ?>">
+	<<?php echo $tag ?> <?php comment_class( empty( $args['has_children'] ) ? '' : "parent comment-list " ) ?> id="comment-<?php comment_ID() ?>">
 	<?php if ( 'div' != $args['style'] ) : ?>
 	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
 	<?php endif; ?>
+	
 	<div class="comment-author vcard">
 	<?php if ( $args['avatar_size'] != 0 ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
 	<?php printf( __( '<div class="fn">%s<span class="says">:</span></div>' ),get_comment_author_link()); ?>
 	</div>
+
 	<?php if ( $comment->comment_approved == '0' ) : ?>
 		<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em>
 		<br />
@@ -179,6 +183,7 @@ function mytheme_comment($comment, $args, $depth) {
 	<?php if ( 'div' != $args['style'] ) : ?>
 	</div>
 	<?php endif; ?>
+
 <?php
 }
 
