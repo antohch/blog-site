6e13f81a56d6941ed7807d3003ed186a3b42b7ed
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-12-30 15:40:01 +0300

    Сделал резиновое меню

--- wp-content/themes/theme1/header.php
+++ wp-content/themes/theme1/header.php
@@ -7,47 +7,47 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/responsiveslides.min.js"></script>
 <script>
-		$(function() {
-			$(".rslides").responsiveSlides({
-			  auto: true,             // Boolean: Animate automatically, true or false
-			  speed: 500,            // Integer: Speed of the transition, in milliseconds
-			  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-			  pager: true,           // Boolean: Show pager, true or false
-			  nav: false,             // Boolean: Show navigation, true or false
-			  random: false,          // Boolean: Randomize the order of the slides, true or false
-			  pause: false,           // Boolean: Pause on hover, true or false
-			  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
-			  prevText: "",   // String: Text for the "previous" button
-			  nextText: "",       // String: Text for the "next" button
-			  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
-			  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
-			  manualControls: "",     // Selector: Declare custom pager navigation
-			  namespace: "rslides",   // String: Change the default namespace used
-			  before: function(){},   // Function: Before callback
-			  after: function(){}     // Function: After callback
-			  
-			  
-			  
-			  
-			  
-			var pull 		= $('#pull');
-				menu 		= $('.menu-main ul');
-				menuHeight	= menu.height();
+	$(function() {
+		$(".rslides").responsiveSlides({
+			auto: true,             // Boolean: Animate automatically, true or false
+			speed: 500,            // Integer: Speed of the transition, in milliseconds
+			timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+			pager: true,           // Boolean: Show pager, true or false
+			nav: false,             // Boolean: Show navigation, true or false
+			random: false,          // Boolean: Randomize the order of the slides, true or false
+			pause: false,           // Boolean: Pause on hover, true or false
+			pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+			prevText: "",   // String: Text for the "previous" button
+			nextText: "",       // String: Text for the "next" button
+			maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+			navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+			manualControls: "",     // Selector: Declare custom pager navigation
+			namespace: "rslides",   // String: Change the default namespace used
+			before: function(){},   // Function: Before callback
+			after: function(){}     // Function: After callback
+	});
+		  
+		  
+		  
+		  
+		var pull 		= $('#pull');
+			menu 		= $('.menu-main ul');
+			menuHeight	= menu.height();
 
-			$(pull).on('click', function(e) {
-				e.preventDefault();
-				menu.slideToggle();
-			});
-
-			$(window).resize(function(){
-        		var w = $(window).width();
-        		if(w > 320 && menu.is(':hidden')) {
-        			menu.removeAttr('style');
-        		}
-    		});
-			});
-			
+		$(pull).on('click', function(e) {
+			e.preventDefault();
+			menu.slideToggle();
 		});
+
+		$(window).resize(function(){
+			var w = $(window).width();
+			if(w > 320 && menu.is(':hidden')) {
+				menu.removeAttr('style');
+			}
+		
+		});
+		
+	});
 </script>
 </head>
 
