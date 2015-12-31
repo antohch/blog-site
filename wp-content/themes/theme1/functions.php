aa2cb52d78444e1e5240a65c61aae8d8d3dbdf5a
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-12-31 11:03:42 +0300

    js подключил безопасно

--- wp-content/themes/theme1/functions.php
+++ wp-content/themes/theme1/functions.php
@@ -4,8 +4,7 @@
 Загружаемые скрипты и стили
 */
 function load_style_script(){
-	wp_enqueue_script('jquery_my', get_template_directory_uri() . '/js/jquery-1.10.1.min.js');
-	wp_enqueue_script('jqFancyTransitions', get_template_directory_uri() . '/js/jqFancyTransitions.1.8.min.js');
+	wp_enqueue_script('responsiveslides-min', get_template_directory_uri() . '/js/responsiveslides.min.js');
 	wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
 }
 
--- wp-content/themes/theme1/header.php
+++ wp-content/themes/theme1/header.php
@@ -5,51 +5,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php bloginfo('name'); wp_title();?></title>
 <?php wp_head();?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/js/responsiveslides.min.js"></script>
-<script>
-	$(function() {
-		$(".rslides").responsiveSlides({
-			auto: true,             // Boolean: Animate automatically, true or false
-			speed: 500,            // Integer: Speed of the transition, in milliseconds
-			timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-			pager: true,           // Boolean: Show pager, true or false
-			nav: false,             // Boolean: Show navigation, true or false
-			random: false,          // Boolean: Randomize the order of the slides, true or false
-			pause: false,           // Boolean: Pause on hover, true or false
-			pauseControls: true,    // Boolean: Pause when hovering controls, true or false
-			prevText: "",   // String: Text for the "previous" button
-			nextText: "",       // String: Text for the "next" button
-			maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
-			navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
-			manualControls: "",     // Selector: Declare custom pager navigation
-			namespace: "rslides",   // String: Change the default namespace used
-			before: function(){},   // Function: Before callback
-			after: function(){}     // Function: After callback
-	});
-		  
-		  
-		  
-		  
-		var pull 		= $('#pull');
-			menu 		= $('.menu-main .menu');
-			menuHeight	= menu.height();
 
-		$(pull).on('click', function(e) {
-			e.preventDefault();
-			menu.slideToggle();
-		});
 
-		$(window).resize(function(){
-			var w = $(window).width();
-			if(w > 320 && menu.is(':hidden')) {
-				menu.removeAttr('style');
-			}
-		
-		});
-		
-	});
-</script>
 </head>
 
 <body>
